2dfe5f74fc5ffaa2ef79c44272764ed4ad42ce6a
AndresEdu <l19141117@queretaro.tecnm.mx>
2022-10-05 08:37:14 -0500

    Archivos iniciales

---
+++ src/main/java/com/cine/service/CineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cine.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cine/service/CineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
